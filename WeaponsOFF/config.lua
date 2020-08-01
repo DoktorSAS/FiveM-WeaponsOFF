8740035f5349be5ebda40cf6a56e4b5b3400005c
DoktorSAS <62484597+DoktorSAS@users.noreply.github.com>
2020-08-01 10:46:19 +0200

    Add files via upload

---
+++ WeaponsOFF/config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.NPCWeaponDrop = false -- Set it to true if want enable NPC Weapon Drop, set it to false if you want Disable NPC Weapons Drop
+Config.CARWeaponGive = false -- Set it to true if want enable CARs Weapon Give, set it to false if you want Disable CARs Weapons Drop
